c7b3c162ba6ebf2a63cf4718bb4bd63a9b06fb5c
Fabio <josefabiosan@gmail.com>
2023-06-26 23:13:04 -0300

    classes

--- bin/app_dio_oo.dart
+++ bin/app_dio_oo.dart
@@ -2,5 +2,6 @@ import 'package:app_dio_oo/classes/pessoa.dart';
 
 void main(List<String> arguments) {
   var p1 = Pessoa();
-  print(p1);
+  p1.nome = "Danilo";
+  print(p1.nome);
 }
--- lib/classes/pessoa.dart
+++ lib/classes/pessoa.dart
@@ -1,1 +1,4 @@
-class Pessoa {}
+class Pessoa {
+  String nome = "";
+  String endereco = "";
+}
