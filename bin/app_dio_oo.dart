88d4830bceb31e0d9605686bb4bd9b1c10e196ba
Fabio <josefabiosan@gmail.com>
2023-06-27 22:49:43 -0300

    POO

--- bin/app_dio_oo.dart
+++ bin/app_dio_oo.dart
@@ -2,6 +2,10 @@ import 'package:app_dio_oo/classes/pessoa.dart';
 
 void main(List<String> arguments) {
   var p1 = Pessoa();
-  p1.nome = "Danilo";
-  print(p1.nome);
+  p1.setNome("Danilo");
+  p1.setEndereco("Rua 1");
+  print(p1.getNome());
+  print(p1.getEndereco());
+  p1.setNome("Fabio");
+  print(p1.getNome());
 }
--- lib/classes/pessoa.dart
+++ lib/classes/pessoa.dart
@@ -1,4 +1,20 @@
 class Pessoa {
-  String nome = "";
-  String endereco = "";
+  String _nome = "";
+  String _endereco = "";
+
+  void setNome(String nome) {
+    _nome = nome;
+  }
+
+  String getNome() {
+    return _nome;
+  }
+
+  void setEndereco(String endereco) {
+    _endereco = endereco;
+  }
+
+  String getEndereco() {
+    return _endereco;
+  }
 }
