8cd688c7bf93b33d4847b68e076b428a134fd3c9
Fabio <josefabiosan@gmail.com>
2023-07-30 20:40:32 -0300

    estudando tratamento de erro

--- bin/app_dio_oo.dart
+++ bin/app_dio_oo.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:app_dio_oo/classes/pessoa.dart';
 
 void main(List<String> arguments) {
@@ -9,4 +11,19 @@ void main(List<String> arguments) {
   //p1.setNome("Fabio");
   //print(p1.getNome());
   print(p1);
+  print("digite um numero inteiro");
+  bool condicao = false;
+  while (!condicao) {
+    try {
+      int line = int.parse(lerConsole("Digite um nome"));
+      print("O numero inteiro informado é $line");
+      condicao = true;
+    } catch (e) {
+      print("Dado inserido incorreto, digite um numero!");
+    }
+  }
+}
+
+String lerConsole(texto) {
+  return stdin.readLineSync()!;
 }
