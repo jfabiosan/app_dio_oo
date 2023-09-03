ae6633b7a43c93fccbf4972d379b878d241b72a4
Fabio <josefabiosan@gmail.com>
2023-09-03 19:01:16 -0300

    Revisando exercico de tratamento de excecao

--- bin/app_dio_oo.dart
+++ bin/app_dio_oo.dart
@@ -4,26 +4,31 @@ import 'package:app_dio_oo/classes/pessoa.dart';
 
 void main(List<String> arguments) {
   var p1 = Pessoa("Danilo", "Rua 1");
-  //p1.setNome("Danilo");
-  //p1.setEndereco("Rua 1");
-  //print(p1.getNome());
-  //print(p1.getEndereco());
-  //p1.setNome("Fabio");
-  //print(p1.getNome());
+  p1.setNome("Danilo");
+  p1.setEndereco("Rua 1");
+  print(p1.getNome());
+  print(p1.getEndereco());
+  p1.setNome("Fabio");
+  print(p1.getNome());
   print(p1);
-  print("digite um numero inteiro");
+
+//Exercico para tratamento de excecao usando o bloco try catch
   bool condicao = false;
   while (!condicao) {
+    //try catch para tratamento de exceção
     try {
-      int line = int.parse(lerConsole("Digite um nome"));
+      //variavel que armazena o valor digitado e converte de String para int
+      int line = int.parse(lerConsole("Digite um numero inteiro"));
       print("O numero inteiro informado é $line");
       condicao = true;
     } catch (e) {
-      print("Dado inserido incorreto, digite um numero!");
+      print("Dado inserido incorreto!");
     }
   }
 }
 
-String lerConsole(texto) {
+//Funcao para solicitar ao usuario a digitacao dos dados
+String lerConsole(String texto) {
+  print(texto);
   return stdin.readLineSync()!;
 }
